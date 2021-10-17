8ab7392a66a7aff129b266e478e4aff6be289831
Marquesa Asmussen <marquesa_a@yahoo.com>
2021-10-17 01:09:52 -0700

    add challenge 10 solutions

--- javascript/code-challenges/challenges-10.test.js
+++ javascript/code-challenges/challenges-10.test.js
@@ -182,7 +182,15 @@ Here is a sample board:
 The top row of the board is considered row zero and row numbers increase as they go down.
 ------------------------------------------------------------------------------------------------ */
 
-const battleship = (board, row, col) => {};
+const battleship = (board, row, col) => {
+  board.map();
+
+  if ("#") {
+    return hit;
+  } else {
+    return miss;
+  }
+};
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 8 - Stretch Goal
