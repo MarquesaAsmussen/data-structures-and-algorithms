63945ecdfcd0cf15411dc56e4298f474d9c0dd57
Marquesa Asmussen <marquesa_a@yahoo.com>
2021-10-17 00:56:46 -0700

    add day 10 challenges

--- javascript/code-challenges/challenges-10.test.js
+++ javascript/code-challenges/challenges-10.test.js
@@ -192,9 +192,7 @@ Write a function named calculateProduct that takes in a two-dimensional array of
 For example, the following input returns a product of 720: [[1,2], [3,4], [5,6]]
 ------------------------------------------------------------------------------------------------ */
 
-const calculateProduct = (numbers) => {
-  // Solution code here...
-};
+const calculateProduct = (numbers) => {};
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 9 - Stretch Goal
