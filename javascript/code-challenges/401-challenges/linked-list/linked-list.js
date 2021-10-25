54c1ce28016d5740683d072ad3a771d08e957ac9
Marquesa Asmussen <marquesa_a@yahoo.com>
2021-10-25 16:39:09 -0700

    add start of challenge 5

--- javascript/code-challenges/401-challenges/linked-list/linked-list.js
+++ javascript/code-challenges/401-challenges/linked-list/linked-list.js
@@ -1,24 +1,32 @@
-// Node
+"use strict";
+
+// "Node"
 // Create a Node class that has properties for the value stored in the Node, and a pointer to the next Node.
-// Linked List
-// Create a Linked List class
-// Within your Linked List class, include a head property.
-// Upon instantiation, an empty Linked List should be created.
-// The class should contain the following methods
-// insert
-// Arguments: value
-// Returns: nothing
-// Adds a new node with that value to the head of the list with an O(1) Time performance.
-// includes
-// Arguments: value
-// Returns: Boolean
-// Indicates whether that value exists as a Node’s value somewhere within the list.
-// to string
-// Arguments: none
-// Returns: a string representing all the values in the Linked List, formatted as:
-// "{ a } -> { b } -> { c } -> NULL"
-// Any exceptions or errors that come from your code should be semantic, capture-able errors. For example, rather than a default error thrown by your language, your code should raise/throw a custom, semantic error that describes what went wrong in calling the methods you wrote for this lab.
-// Be sure to follow your language/frameworks standard naming conventions (e.g. C# uses PascalCasing for all method and class names).
+
+class Node {
+  constructor(value) {}
+}
+
+// "Linked List"
+// -Create a Linked List class
+// -Within your Linked List class, include a head property.
+//    -Upon instantiation, an empty Linked List should be created.
+// -The class should contain the following methods
+//    -insert
+//        -Arguments: value
+//        -Returns: nothing
+//        -Adds a new node with that value to the head of the list with an O(1) Time performance.
+//    -includes
+//        -Arguments: value
+//        -Returns: Boolean
+//            -Indicates whether that value exists as a Node’s value somewhere within the list.
+//    -to string
+//        -Arguments: none
+//        -Returns: a string representing all the values in the Linked List, formatted as:
+//          -`"{ a } -> { b } -> { c } -> NULL"`
+// -Any exceptions or errors that come from your code should be semantic, capture-able errors. For example, rather than a default error thrown by your language, your code should raise/throw a custom, semantic error that describes what went wrong in calling the methods you wrote for this lab.
+// -Be sure to follow your language/frameworks standard naming conventions (e.g. C# uses PascalCasing for all method and class names).
+
 // Structure and Testing
 // Utilize the Single-responsibility principle: any methods you write should be clean, reusable, abstract component parts to the whole challenge. You will be given feedback and marked down if you attempt to define a large, complex algorithm in one function definition.
 
