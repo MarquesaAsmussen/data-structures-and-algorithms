b3423d603c4280caab0b26b0104a229ca3a86c11
Marquesa Asmussen <marquesa_a@yahoo.com>
2021-10-25 19:09:40 -0700

    update framework

--- javascript/code-challenges/401-challenges/linked-list/linked-list.js
+++ javascript/code-challenges/401-challenges/linked-list/linked-list.js
@@ -4,26 +4,60 @@
 // Create a Node class that has properties for the value stored in the Node, and a pointer to the next Node.
 
 class Node {
-  constructor(value) {}
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 
 // "Linked List"
 // -Create a Linked List class
 // -Within your Linked List class, include a head property.
 //    -Upon instantiation, an empty Linked List should be created.
-// -The class should contain the following methods
-//    -insert
-//        -Arguments: value
-//        -Returns: nothing
-//        -Adds a new node with that value to the head of the list with an O(1) Time performance.
-//    -includes
-//        -Arguments: value
-//        -Returns: Boolean
-//            -Indicates whether that value exists as a Node’s value somewhere within the list.
-//    -to string
-//        -Arguments: none
-//        -Returns: a string representing all the values in the Linked List, formatted as:
-//          -`"{ a } -> { b } -> { c } -> NULL"`
+
+class LinkedList {
+  constructor(head) {
+    this.head = null;
+  }
+
+  transversIterative() {
+    let current = this.head;
+    while (current) {
+      console.log(current.value);
+      current = current.next;
+    }
+  }
+
+  // -The class should contain the following methods
+  //    -insert
+  //        -Arguments: value
+  //        -Returns: nothing
+  //        -Adds a new node with that value to the head of the list with an O(1) Time performance.
+
+  insert(value) {}
+
+  //    -includes
+  //        -Arguments: value
+  //        -Returns: Boolean
+  //            -Indicates whether that value exists as a Node’s value somewhere within the list.
+
+  includes(value) {}
+
+  //    -to string
+  //        -Arguments: none
+  //        -Returns: a string representing all the values in the Linked List, formatted as:
+  //          -`"{ a } -> { b } -> { c } -> NULL"`
+
+  toString() {}
+}
+
+const list = new LinkedList();
+
+list.head = new Node();
+list.head.next = new Node();
+list.head.next.next = new Node();
+list.head.next.next.next = new Node();
+
 // -Any exceptions or errors that come from your code should be semantic, capture-able errors. For example, rather than a default error thrown by your language, your code should raise/throw a custom, semantic error that describes what went wrong in calling the methods you wrote for this lab.
 // -Be sure to follow your language/frameworks standard naming conventions (e.g. C# uses PascalCasing for all method and class names).
 
